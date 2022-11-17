3f9905a5f96943951df0a9f3d1fe8f5882d6a086
Darnion <63425889+Darnion@users.noreply.github.com>
2022-11-17 18:48:36 +0300

    Add files via upload

---
+++ NuGet/NuGet_WF.cs
@@ -0,0 +1,35 @@
+﻿namespace AdmComDGVNuGet
+{
+    public class NuGet_WF
+    {
+        public NuGet_WF()
+        {
+
+        }
+
+        public void Add<T>(T args, ref List<T> entrants)
+        {
+            entrants.Add(args);
+        }
+        public void Remove<T>(T args, ref List<T> entrants)
+        {
+            entrants.Remove(args);
+        }
+
+        public void Change<T>(T oldValues, T args, ref List<T> entrants)
+        {
+            var index = entrants.IndexOf(oldValues);
+            entrants[index] = args;
+        }
+
+        public void FillEnum(Enum item, ref ComboBox comboBox)
+        {
+            Type type = item.GetType();
+
+            foreach (var value in Enum.GetValues(type))
+            {
+                comboBox.Items.Add(value);
+            }
+        }
+    }
+}
